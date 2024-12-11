25ec73a79af170459f8b20d010dc08651b45188f
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2024-12-11 08:46:20 +0100

    Ejercicio 2 (avanzado) terminado.

--- src/ejerciciosstring/Ejer2Avanzado.java
+++ src/ejerciciosstring/Ejer2Avanzado.java
@@ -24,9 +24,8 @@ public class Ejer2Avanzado {
 		// Le ponemos al array la longitud de la contraseña
 		pswd = new char[password.length()];
 		
-		for (int i = 0; i < password.length(); i++) {
-			pswd[i] = password.charAt(i);
-		}
+		// Rellenamos el array pswd con asteriscos
+		Arrays.fill(pswd, '*');
 		
 		// Bucle Do-While
 		do {
@@ -34,10 +33,21 @@ public class Ejer2Avanzado {
 			System.out.print("Jugador 2, adivina la contraseña: ");
 			passwordTry = reader.nextLine();
 			
+			/* Comparamos el intento de contraseña con la contraseña real, sustituyendo
+			 * en el array pswd los asteriscos por las letras correspondientes que se hayan
+			 * acertado */
+			for (int i = 0; i < pswd.length; i++) {
+				if (passwordTry.charAt(i) == password.charAt(i))
+					pswd[i] = passwordTry.charAt(i);
+			}
 			
+			// Imprimimos el array
+			System.out.println(Arrays.toString(pswd));
 		} while (!password.equals(passwordTry)); // Mientras el intento sea desigual a la contraseña
 		
 		reader.close(); // Cerramos el Scanner
+		
+		System.out.print("Has acertado la contraseña.");
 	}
 
 }
