9762e60cc9304984916329925f60b8869521a6cd
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2024-12-18 09:41:02 +0100

    Ejercicio 13 NO terminado.

---
+++ src/ejerciciosstring/Ejer13.java
@@ -0,0 +1,18 @@
+package ejerciciosstring;
+
+public class Ejer13 {
+	
+	static boolean anagrama(String word1, String word2) {
+		boolean anagrama = false;
+		
+		
+		
+		return anagrama;
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
