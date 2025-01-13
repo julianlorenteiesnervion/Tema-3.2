08862bf4233440c890708c9b8830716679ad1aab
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2025-01-13 12:42:21 +0100

    Ejercicio 15 NO terminado.

---
+++ src/ejerciciosstring/Ejer15.java
@@ -0,0 +1,27 @@
+package ejerciciosstring;
+
+import java.util.*;
+
+public class Ejer15 {
+
+	static String randomOrder(String phrase) {
+		String randomizedPhrase;
+		
+		
+		
+		
+		return randomizedPhrase;
+	}
+	
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		
+		String word;
+		
+		System.out.print("Jugador 1, introduce una palabra: ");
+		word = reader.next();
+		
+
+	}
+
+}
